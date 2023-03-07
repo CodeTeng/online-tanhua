ed772fe075e2c7ee9482cdb8163374a61e0c1432
teng <teng2002823@outlook.com>
2023-03-07 11:46:45 +0800

    realization gateway function

---
+++ tanhua-gateway/src/main/java/com/tanhua/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.tanhua.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: ~Teng~
+ * @date: 2023/3/7 11:38
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ tanhua-gateway/src/main/java/com/tanhua/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.tanhua.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @description: 网关跨域配置
+ * @author: ~Teng~
+ * @date: 2023/3/7 11:38
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
